f3b9acc180c302125cc8ff35ae0eeb6fbfcb0f8f
kajocudina-cloud <kajo.cudina@gmail.com>
2026-02-11 17:18:20 -0500

    Complete Redux integration: Store configuration, CartItem logic, and ProductList updates

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './CartSlice';
 
+// Configuring the Redux Store
 const store = configureStore({
+    // The reducer key defines how different pieces of state are managed
     reducer: {
+        // Assigning cartReducer to manage the 'cart' slice of the state
         cart: cartReducer,
     },
 });
